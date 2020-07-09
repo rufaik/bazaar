8b92442af7c6a2ee723ea2e0e74bb676809dc3fb
Kems <rufai@hotmail.co.uk>
2020-07-09 16:57:48 +0100

    mobilenav

--- src/Nav.js
+++ src/Nav.js
@@ -10,17 +10,16 @@ import { Fade } from 'react-slideshow-image';
 
 const Nav = () => {
   return (
-  	<div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white bb frontfont b--red shadow-5">
-  <a class="navbar-brand" href="/Home"><img  alt="D's Bazaar"
-                    src='./JDBAZ.png' className ='w5'/></a>
   
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white bb frontfont b--red shadow-5">
+<a class="navbar-brand" href="/Home"><img  alt="D's Bazaar"
+                    src='./JDBAZ.png' className ='w5'/></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navfont navbar-nav">
-      
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
       <li class="nav-item">
         <a class="nav-link  fw6" href="#1">SERVICES</a>
       </li>      
@@ -30,19 +29,21 @@ const Nav = () => {
       <li class="nav-item">
         <a class="nav-link fw6" href="#2">OPENING TIMES</a>
       </li>
-
-      <a href="#4" className="lefty  mr4">
+    </ul>
+    <form class="form-inline vanalign my-2 my-lg-0">
+      <a href="#4" className=" ">
         <img  alt='DBAZZAR' src='./van.png' className ='w3'/>
         <img  alt='DBAZZAR' src='./gap.png' className ='w1'/>
-        <button type="button" class="bun fw6 btn btn-primary btn-lg">BOOK NOW</button>
       </a>
+        <button type="button" class="bun fw6 btn btn-primary btn-lg">BOOK NOW</button>
+    </form>
 
-
-    </ul>
   </div>
 </nav>
-    </div>
+   
   )
 }
 
 export default Nav;
+
+
